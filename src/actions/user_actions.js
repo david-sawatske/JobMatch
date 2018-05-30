82e75b70aafbc5b9f7d392a5651404e162a79c12
David Sawatske <sawatskeda10@gmail.com>
2018-05-30 14:57:16 -0500

    Create updateResumeImageData Action Creator

--- src/actions/user_actions.js
+++ src/actions/user_actions.js
@@ -1,6 +1,12 @@
 export const UPDATE_PERSONAL_DATA = 'UPDATE_PERSONAL_DATA';
+export const UPDATE_RESUME_IMAGE_DATA = 'UPDATE_RESUME_IMAGE_DATA';
 
 export const updatePersonalData = data => ({
   type: UPDATE_PERSONAL_DATA,
+  data
+});
+
+export const updateResumeImageData = data => ({
+  type: UPDATE_RESUME_IMAGE_DATA,
   data
 });
