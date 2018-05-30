bb6e5696aa4a7de05e53ebe4deb24dca7cffda70
David Sawatske <sawatskeda10@gmail.com>
2018-05-30 11:06:01 -0500

    Create Personal Info Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      first: '',
+      last: '',
+      github: '',
+      linkedIn: ''
+    }
+
+    this.update = this.update.bind(this);
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h1>Test</h1>
+      <div className="App">
+        <form>
+          <label>First Name</label>
+          <input type="text" value={this.state.first} onChange={this.update('first')}/>
+          <br/>
+          <label>Lase Name</label>
+          <input type="text" value={this.state.last} onChange={this.update('last')}/>
+          <br/>
+          <label>GitHub Profile</label>
+          <input type="text" value={this.state.github} onChange={this.update('github')}/>
+          <br/>
+          <label>LinkedIn Profile</label>
+          <input type="text" value={this.state.linkedIn} onChange={this.update('linkedIn')}/>
+        </form>
       </div>
     );
   }
