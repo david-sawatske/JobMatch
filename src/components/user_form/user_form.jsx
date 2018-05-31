6fff597a8e8e5e939f1a15d84c3eddf1e42533da
David Sawatske <sawatskeda10@gmail.com>
2018-05-31 16:36:43 -0500

    Add switch to assign currentComponent and create header to set value

--- src/components/user_form/user_form.jsx
+++ src/components/user_form/user_form.jsx
@@ -1,24 +1,61 @@
 import React, { Component } from 'react';
+
+import UserFormHeader from './user_form_header'
 import PersonalInfo from './personal_info'
 import Technologies from './technologies'
 import ResumePhoto from './resume_photo'
 import Locations from './locations'
-import Salary from './salary'
 import Benefits from './benefits'
+import Salary from './salary'
 
 export default class UserForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeComponentIdx: 0
+    };
+  }
+
+  setActiveComponentIdx = (idx) => {
+    this.setState({ activeComponentIdx: idx });
+  }
+
   render() {
-    const { updatePersonalData, updateResumeImageData, updateTechsData,
-            updateLocationsData, updateBenefitsData, updateSalaryData } = this.props;
+    const { activeComponentIdx } = this.state;
+    const { updatePersonalData, updateResumeImageData,
+            updateLocationsData, updateBenefitsData,
+            updateTechsData, updateSalaryData } = this.props;
+
+   let currentComponent
+   switch (activeComponentIdx) {
+     case 0:
+       currentComponent = <PersonalInfo updatePersonalData={updatePersonalData} />
+       break;
+     case 1:
+       currentComponent = <ResumePhoto updateResumeImageData={updateResumeImageData} />
+       break;
+     case 2:
+       currentComponent = <Locations updateLocationsData={updateLocationsData} />
+       break;
+     case 3:
+       currentComponent = <Technologies updateTechsData={updateTechsData} />
+       break;
+     case 4:
+       currentComponent = <Benefits updateBenefitsData={updateBenefitsData} />
+       break;
+     case 5:
+       currentComponent = <Salary updateSalaryData={updateSalaryData} />
+       break;
+     default:
+      currentComponent = null
+   }
 
     return (
       <div className="user-form">
-        <Salary updateSalaryData={updateSalaryData} />
-        <Technologies updateTechsData={updateTechsData} />
-        <Benefits updateBenefitsData={updateBenefitsData} />
-        <Locations updateLocationsData={updateLocationsData} />
-        <PersonalInfo updatePersonalData={updatePersonalData} />
-        <ResumePhoto updateResumeImageData={updateResumeImageData} />
+        <UserFormHeader activeIdx={activeComponentIdx}
+                        setIdx={this.setActiveComponentIdx} />
+        { currentComponent }
       </div>
     );
   }
---
+++ src/components/user_form/user_form_header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const UserFormHeader = ({ activeIdx, setIdx }) => {
+  const componentHeadings = [ "Personal Info", "CV & Photo",
+                              "Locations", "Skills",
+                              "Benefits", "Salary" ]
+
+  const handleClick = idx => {
+    setIdx(idx)
+  }
+
+  return (
+    <div>
+      { componentHeadings.map((heading, idx) => {
+        const compClass = (idx === activeIdx) ? "current-heading" : "heading";
+
+        return (
+          <h2 key={idx}
+              className={compClass}
+              onClick={() => handleClick(idx)}>
+            {heading}
+          </h2>
+        )
+      })}
+    </div>
+  )
+};
+
+export default UserFormHeader;
