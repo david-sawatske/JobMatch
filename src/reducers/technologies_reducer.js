c7ff1ac43b168eee06f2387cea4c3c96497c2b17
David Sawatske <sawatskeda10@gmail.com>
2018-06-10 20:34:07 -0500

    Add default state values for Technologies

--- src/reducers/technologies_reducer.js
+++ src/reducers/technologies_reducer.js
@@ -4,7 +4,24 @@ import { UPDATE_TECHS_DATA } from '../actions/user_actions';
 
 import { merge, union } from 'lodash';
 
-const techsById = (state = {}, action) => {
+const defaultById = {
+  0: { value: 0, label: 'Java' },
+  1: { value: 1, label: 'Python' },
+  3: { value: 3, label: 'C++' },
+  4: { value: 4, label: 'JavaScript' },
+  5: { value: 5, label: 'Ruby on Rails' },
+  6: { value: 6, label: 'jQuery' },
+  7: { value: 7, label: 'HTML' },
+  8: { value: 8, label: 'CSS' },
+  9: { value: 9, label: 'SQL' },
+  10: { value: 10, label: 'React' },
+  11: { value: 11, label: 'Node.js' },
+  12: { value: 12, label: 'AngularJS' }
+}
+
+const defaultIds = [...Array(13).keys()]
+
+const techsById = (state = defaultById, action) => {
   switch(action.type) {
     case UPDATE_TECHS_DATA:
       return merge({}, state, getTechsData(action.data).byId);
@@ -13,7 +30,7 @@ const techsById = (state = {}, action) => {
   }
 };
 
-const allTechIds = (state = {}, action) => {
+const allTechIds = (state = defaultIds, action) => {
   switch(action.type) {
     case UPDATE_TECHS_DATA:
       return union([], state, getTechsData(action.data).allIds);
