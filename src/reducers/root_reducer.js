b69e1539632b1d9fd0ab717ff49fe8ae367c09d6
David Sawatske <sawatskeda10@gmail.com>
2018-06-11 19:07:42 -0500

    Create SkillsReducer and add to RootReducer

--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
 import UserReducer from './user_reducer';
+import SkillsReducer from './skill_reducer';
 import TechnologiesReducer from './technologies_reducer';
 
 const RootReducer = combineReducers({
   technologiesData: TechnologiesReducer,
+  skillsData: SkillsReducer,
   userData: UserReducer
 });
 
---
+++ src/reducers/skill_reducer.js
@@ -0,0 +1,48 @@
+import { combineReducers } from 'redux';
+
+import { UPDATE_SKILL_DATA, CREATE_SKILL_DATA } from '../actions/user_actions';
+
+import { merge, union } from 'lodash';
+
+const skillsById = (state = {}, action) => {
+  switch(action.type) {
+    case UPDATE_SKILL_DATA:
+    case CREATE_SKILL_DATA:
+      return merge({}, state, getSkillsData(action.data).byId);
+    default:
+      return state;
+  }
+};
+
+const allSkillIds = (state = [], action) => {
+  switch(action.type) {
+    case UPDATE_SKILL_DATA:
+    case CREATE_SKILL_DATA:
+      return union([], state, getSkillsData(action.data).allIds);
+    default:
+      return state;
+  }
+};
+
+// START selectors //
+const getSkillsData = obj => {
+  let byId = {};
+  let allIds = [];
+
+  Object.values(obj.selectedTechs).map(tech => {
+    const techId = tech.value;
+
+    byId = merge({}, byId, {[techId]: tech.label });
+    allIds = union([], allIds, [techId]);
+  })
+
+  return { byId: byId, allIds: allIds }
+}
+// END selectors //
+
+const SkillsReducer = combineReducers({
+  allSkillIds,
+  skillsById
+});
+
+export default SkillsReducer;
